d2fb1deac71e173df5cf6c0c863c38635b4a50f1
JJJJJJack <97jack@live.it>
2022-07-14 01:29:17 +0200

    ADD: TopMenu stiky navbar setup for inheritance to children components

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,33 +1,31 @@
-import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import TopMenu from './components/TopMenu';
+import logo from './logo.svg';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TopMenu />} >
-          <Route path="app" element={
-            <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.tsx</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </header>
-          </div>
+        <Route element={<TopMenu />} >
+          <Route path="/" element={
+            <>
+              <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                  Learn React
+                </a>
+              </header>
+              </div>
+            </>
           } />
         </Route>
+        <Route path="*" element={<h1>404 page not found</h1>} /> {/* when no route match the URL-slug */}
       </Routes>
     </BrowserRouter>
   );
