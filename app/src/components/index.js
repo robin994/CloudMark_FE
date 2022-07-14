b6bf33dec0fd4eea1bbb0b00fe929f87febc3d0d
JJJJJJack <97jack@live.it>
2022-07-14 13:49:15 +0200

    ADD: Dashboard component

--- app/src/components/index.js
+++ app/src/components/index.js
@@ -3,5 +3,6 @@ import Login from "./Log_In"
 import Registrazione from "./Registrazione"
 import TopMenu from "./TopMenu"
 import SuperUser from "./Super_User"
+import Dashboard from "./Dashboard"
 
-export { CercaDipendente, Login, Registrazione, TopMenu, SuperUser }
+export { CercaDipendente, Login, Registrazione, TopMenu, SuperUser, Dashboard }
