ad03e8770d517532ff666b023c2daa4e71920aad
JJJJJJack <97jack@live.it>
2022-07-14 13:41:58 +0200

    ADD: index.js for clean import

---
+++ app/src/components/index.js
@@ -0,0 +1,7 @@
+import CercaDipendente from "./Cerca_Dipendente"
+import Login from "./Log_In"
+import Registrazione from "./Registrazione"
+import TopMenu from "./TopMenu"
+import SuperUser from "./Super_User"
+
+export { CercaDipendente, Login, Registrazione, TopMenu, SuperUser }
