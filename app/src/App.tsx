c3f442e24f047c92d7caec5e74bf9dc14a3fa558
JJJJJJack <97jack@live.it>
2022-07-14 12:50:00 +0200

    ADD: signup route renders Registrazione

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,19 +4,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TopMenu from "./components/TopMenu";
-import logo from "./logo.svg";
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<TopMenu />} />
-        <Route path="*" element={<h1>404 page not found</h1>} />{" "}
-        {/* when no route match the URL-slug */}
-        
+        <Route element={<TopMenu />}>
+          <Route path="signup" element={<Registrazione />} />
+        </Route>
+        <Route path="*" element={<h1>404 page not found</h1>} />{/* when no route match the URL-slug */}
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
-
-export default App;
