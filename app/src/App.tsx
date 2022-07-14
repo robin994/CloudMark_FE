281d412ede5e9b83a00445575b2bf460466a8087
JJJJJJack <97jack@live.it>
2022-07-14 13:41:40 +0200

    tmp

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,14 +3,16 @@ import Registrazione from "./components/Registrazione";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import TopMenu from "./components/TopMenu";
+import { TopMenu, Dash } from "./components";
 
+<Dashboard />
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<TopMenu />}>
           <Route path="signup" element={<Registrazione />} />
+          <Route />
         </Route>
         <Route path="*" element={<h1>404 page not found</h1>} />{/* when no route match the URL-slug */}
       </Routes>
