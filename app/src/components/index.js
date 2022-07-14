49ed2aafa25985cb2480e24d2d073aa0c7162e1f
JJJJJJack <97jack@live.it>
2022-07-14 16:22:12 +0200

    FIX: name of component

--- app/src/components/index.js
+++ app/src/components/index.js
@@ -1,5 +1,5 @@
 import CercaDipendente from "./Cerca_Dipendente"
-import Login from "./Log_In"
+import Login from "./Login"
 import Registrazione from "./Registrazione"
 import TopMenu from "./TopMenu"
 import SuperUser from "./Super_User"
