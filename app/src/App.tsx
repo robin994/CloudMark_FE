edf2f3727646a27469f5bbbb00c1f4f80a775ee4
Lorenzo <lorprofessional@gmail.com>
2022-07-14 12:24:39 +0200

    addAzienda and modAziendaAdmin ver 1.0

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { addAziendaComponent } from './components/addAzienda';
+import { modAziendaAdmin } from './components/modAziendaAdmin';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {modAziendaAdmin()}
     </div>
   );
 }
