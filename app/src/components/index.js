f26dd074d635f6f11205ad21621b42540a6c07fb
JJJJJJack <97jack@live.it>
2022-07-14 17:09:03 +0200

    tmp fix

--- app/src/components/index.js
+++ app/src/components/index.js
@@ -1,4 +1,4 @@
-import CercaDipendente from "./Cerca_Dipendente"
+import CercaDipendente from "./CercaDipendente"
 import Login from "./Login"
 import Registrazione from "./Registrazione"
 import TopMenu from "./TopMenu"
@@ -6,3 +6,4 @@ import SuperUser from "./Super_User"
 import Dashboard from "./Dashboard"
 
 export { CercaDipendente, Login, Registrazione, TopMenu, SuperUser, Dashboard }
+
