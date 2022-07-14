6eb4c3f9a81209a9a549a3389845e7daa83844d1
Lorenzo <lamu1197@hotmail.com>
2022-07-14 12:26:07 +0200

    ADD:appjs Components

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import './App.css';
 import Login from './components/Log_In';
+import SuperUser from './components/Super_User';
+import CercaDipendente from './components/Cerca_Dipendente';
 
 
 
@@ -10,7 +12,10 @@ function App() {
     <div className="App">
       <header className="App-header">
       
+      <CercaDipendente/>
+      <SuperUser/>
       <Login/>
+     
       
       </header>
     </div>
