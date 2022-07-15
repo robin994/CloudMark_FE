04f690fe596cbbc01952d3ccd6b54f23ddeea10d
JJJJJJack <97jack@live.it>
2022-07-15 11:56:01 +0200

    ADD: ListaDipendenti import

--- app/src/components/index.js
+++ app/src/components/index.js
@@ -4,6 +4,7 @@ import Registrazione from "./Registrazione"
 import TopMenu from "./TopMenu"
 import SuperUser from "./Super_User"
 import Dashboard from "./Dashboard"
+import ListaDipendenti from "./ListaDipendenti"
 
-export { CercaDipendente, Login, Registrazione, TopMenu, SuperUser, Dashboard }
+export { CercaDipendente, Login, Registrazione, TopMenu, SuperUser, Dashboard, ListaDipendenti }
 
