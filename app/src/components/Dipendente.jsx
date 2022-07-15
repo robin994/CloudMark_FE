8a5fcb49349aad7d941ced8578478aa60df7e501
JJJJJJack <97jack@live.it>
2022-07-15 22:02:56 +0200

    ADD: Dipendente infos

--- app/src/components/Dipendente.jsx
+++ app/src/components/Dipendente.jsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card, ListGroup } from "react-bootstrap";
 import { useNavigate, useParams } from "react-router-dom";
 import { getListaDipendenti } from "../data_mock";
 import CustomCard from "./CustomCard";
@@ -16,7 +16,17 @@ export default function Dipendente() {
     let dipendente = lista_dipendenti[params.id_dipendente]
     return (
         <div className="mycentereddiv">
-            {/* dipendente infos here */}
+            <Card style={{ width: '18rem' }}>
+                <Card.Img src="/statics/dipendente.jpg" />
+                <Card.Header>{`${dipendente.nome} ${dipendente.cognome}`}</Card.Header>
+                <ListGroup variant="flush">
+                    <ListGroup.Item>{`contratto: ${dipendente.tipo_contratto}`}</ListGroup.Item>
+                    <ListGroup.Item>{`email: ${dipendente.email}`}</ListGroup.Item>
+                    <ListGroup.Item>{`telefono: ${dipendente.telefono}`}</ListGroup.Item>
+                    <ListGroup.Item>{`codice fiscale: ${dipendente.cf}`}</ListGroup.Item>
+                    <ListGroup.Item>{`iban: ${dipendente.iban}`}</ListGroup.Item>
+                </ListGroup>
+            </Card>
         </div>
     )
 }
