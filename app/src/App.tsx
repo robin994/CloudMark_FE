3bdd44f88246377d04596613d3b68e9a883c745b
JJJJJJack <97jack@live.it>
2022-07-15 18:02:06 +0200

    ADD: dipendente route with id; FIX: routes trailing slash

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,15 +4,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import * as Comps from "./components";
 
 export default function App() {
+  sessionStorage.auth = "false"
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Comps.TopMenu />}>
-          <Route path="cercadipendente" element={<Comps.CercaDipendente />} />
-          <Route path="signup" element={<Comps.Registrazione />} />
-          <Route path="dashboard" element={<Comps.Dashboard />} />
-          <Route path="superuser" element={<Comps.SuperUser />} />
-          <Route path="lista_dipendenti" element={<Comps.ListaDipendenti />} />
+          <Route path="/" element={<Comps.Dashboard />} />
+          <Route path="/cercadipendente" element={<Comps.CercaDipendente />} />
+          <Route path="/signup" element={<Comps.Registrazione />} />
+          <Route path="/dashboard" element={<Comps.Dashboard />} />
+          <Route path="/superuser" element={<Comps.SuperUser />} />
+          <Route path="/lista_dipendenti" element={<Comps.DipendentiLista />} />
+            <Route path="/dipendente/:id_dipendente" element={<Comps.Dipendente />} />
         </Route>
         <Route path="login" element={<Comps.Login />} />
         <Route path="*" element={<h1>404 page not found</h1>} />{/* when no route match the URL-slug */}
