17041954eed32473910e5a1a50f7568cccb38778
JJJJJJack <97jack@live.it>
2022-07-15 18:02:13 +0200

    Fake json

--- app/src/data_mock.js
+++ app/src/data_mock.js
@@ -1,6 +1,6 @@
 let lista_dipendenti = {
-    "1": {
-        "id_employee": 1,
+    "0": {
+        "id_employee": 0,
         "nome": "bruno",
         "cognome": "rossi",
         "cf": "123",
@@ -9,8 +9,168 @@ let lista_dipendenti = {
         "email": "brunorossi@gmail.com",
         "telefono": "1234"
     },
+    "1": {
+        "id_employee": 1,
+        "nome": "francesco",
+        "cognome": "gialli",
+        "cf": "221",
+        "iban": "333",
+        "tipo_contratto": "indeterminato",
+        "email": "francescogialli@gmail.com",
+        "telefono": "11222"
+    },
     "2": {
         "id_employee": 2,
+        "nome": "francesco",
+        "cognome": "gialli",
+        "cf": "221",
+        "iban": "333",
+        "tipo_contratto": "indeterminato",
+        "email": "francescogialli@gmail.com",
+        "telefono": "11222"
+    },
+    "3": {
+        "id_employee": 3,
+        "nome": "francesco",
+        "cognome": "gialli",
+        "cf": "221",
+        "iban": "333",
+        "tipo_contratto": "indeterminato",
+        "email": "francescogialli@gmail.com",
+        "telefono": "11222"
+    },
+    "4": {
+        "id_employee": 4,
+        "nome": "francesco",
+        "cognome": "gialli",
+        "cf": "221",
+        "iban": "333",
+        "tipo_contratto": "indeterminato",
+        "email": "francescogialli@gmail.com",
+        "telefono": "11222"
+    },
+    "5": {
+        "id_employee": 5,
+        "nome": "francesco",
+        "cognome": "gialli",
+        "cf": "221",
+        "iban": "333",
+        "tipo_contratto": "indeterminato",
+        "email": "francescogialli@gmail.com",
+        "telefono": "11222"
+    },
+    "6": {
+        "id_employee": 6,
+        "nome": "francesco",
+        "cognome": "gialli",
+        "cf": "221",
+        "iban": "333",
+        "tipo_contratto": "indeterminato",
+        "email": "francescogialli@gmail.com",
+        "telefono": "11222"
+    },
+    "7": {
+        "id_employee": 7,
+        "nome": "francesco",
+        "cognome": "gialli",
+        "cf": "221",
+        "iban": "333",
+        "tipo_contratto": "indeterminato",
+        "email": "francescogialli@gmail.com",
+        "telefono": "11222"
+    },
+    "8": {
+        "id_employee": 8,
+        "nome": "francesco",
+        "cognome": "gialli",
+        "cf": "221",
+        "iban": "333",
+        "tipo_contratto": "indeterminato",
+        "email": "francescogialli@gmail.com",
+        "telefono": "11222"
+    },
+    "9": {
+        "id_employee": 9,
+        "nome": "francesco",
+        "cognome": "gialli",
+        "cf": "221",
+        "iban": "333",
+        "tipo_contratto": "indeterminato",
+        "email": "francescogialli@gmail.com",
+        "telefono": "11222"
+    },
+    "10": {
+        "id_employee": 10,
+        "nome": "francesco",
+        "cognome": "gialli",
+        "cf": "221",
+        "iban": "333",
+        "tipo_contratto": "indeterminato",
+        "email": "francescogialli@gmail.com",
+        "telefono": "11222"
+    },
+    "11": {
+        "id_employee": 11,
+        "nome": "francesco",
+        "cognome": "gialli",
+        "cf": "221",
+        "iban": "333",
+        "tipo_contratto": "indeterminato",
+        "email": "francescogialli@gmail.com",
+        "telefono": "11222"
+    },
+    "12": {
+        "id_employee": 12,
+        "nome": "francesco",
+        "cognome": "gialli",
+        "cf": "221",
+        "iban": "333",
+        "tipo_contratto": "indeterminato",
+        "email": "francescogialli@gmail.com",
+        "telefono": "11222"
+    },
+    "13": {
+        "id_employee": 13,
+        "nome": "francesco",
+        "cognome": "gialli",
+        "cf": "221",
+        "iban": "333",
+        "tipo_contratto": "indeterminato",
+        "email": "francescogialli@gmail.com",
+        "telefono": "11222"
+    },
+    "14": {
+        "id_employee": 14,
+        "nome": "francesco",
+        "cognome": "gialli",
+        "cf": "221",
+        "iban": "333",
+        "tipo_contratto": "indeterminato",
+        "email": "francescogialli@gmail.com",
+        "telefono": "11222"
+    },
+    "15": {
+        "id_employee": 15,
+        "nome": "francesco",
+        "cognome": "gialli",
+        "cf": "221",
+        "iban": "333",
+        "tipo_contratto": "indeterminato",
+        "email": "francescogialli@gmail.com",
+        "telefono": "11222"
+    },
+    "16": {
+        "id_employee": 16,
+        "nome": "francesco",
+        "cognome": "gialli",
+        "cf": "221",
+        "iban": "333",
+        "tipo_contratto": "indeterminato",
+        "email": "francescogialli@gmail.com",
+        "telefono": "11222"
+    },
+    "17": {
+        "id_employee": 17,
         "nome": "francesco",
         "cognome": "gialli",
         "cf": "221",
