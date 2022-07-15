fc3c0d83aa6d86f0f5dcd9131d765cb55c1203b9
JJJJJJack <97jack@live.it>
2022-07-15 22:37:27 +0200

    FIX: fake auth system and routes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,14 +4,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import * as Comps from "./components";
 
 export default function App() {
-  sessionStorage.auth = "false"
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Comps.TopMenu />}>
           <Route path="/" element={<Comps.Dashboard />} />
           <Route path="/cercadipendente" element={<Comps.CercaDipendente />} />
-          <Route path="/signup" element={<Comps.Registrazione />} />
           <Route path="/dashboard" element={<Comps.Dashboard />} />
           <Route path="/superuser" element={<Comps.SuperUser />} />
           <Route path="/lista_dipendenti" element={<Comps.DipendentiLista />} />
@@ -23,8 +21,9 @@ export default function App() {
           <Route path="dashboard" element={<Comps.Dashboard />} />
           <Route path="superuser" element={<Comps.SuperUser />} />
         </Route>
-        <Route path="login" element={<Comps.Login />} />
-        <Route path="*" element={<h1>404 page not found</h1>} />{/* when no route match the URL-slug */}
+        <Route path="/signup" element={<Comps.Registrazione />} />
+        <Route path="/login" element={<Comps.Login />} />
+        <Route path="*" element={<h1>404 page not found</h1>} /> {/* when no route match the URL-slug */}
       </Routes>
     </BrowserRouter>
   )
