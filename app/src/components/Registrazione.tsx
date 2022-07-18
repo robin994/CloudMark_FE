c34a61cf03056941028c8ba6cdb7d10d07729d48
francescolillo <francescolillo@hotmail.it>
2022-07-18 10:25:21 +0200

    ADD: validazzioni su i campi email e password di registrazione

--- app/src/components/Registrazione.tsx
+++ app/src/components/Registrazione.tsx
@@ -3,50 +3,52 @@ import React from "react"
 
 
 function Registrazione() {
-
-
+    const submit = () => {
+        let email = document.getElementById("emailRegistrazione") as HTMLInputElement
+        let psw = document.getElementById("pswRegistrazione") as HTMLInputElement
+    }
 
     return (
 
         <>
+            <div className="log-div">
+                <h1>Registrati</h1>
+                <form action="">
 
-            <h1>Registrati</h1>
-            <form action="">
+                    <div>
+                        <input type="text" name="nome" placeholder="digita nome" className="form-control Dim" />
+                    </div>
 
-                <div>
-                    <input type="text" name="nome" placeholder="digita nome" className="form-control Dim" />
-                </div>
+                    <div>
+                        <input type="text" name="cognome" placeholder="digita cognome" className="form-control Dim" />
+                    </div>
 
-                <div>
-                    <input type="text" name="cognome" placeholder="digita cognome" className="form-control Dim" />
-                </div>
+                    <div>
+                        <input type="text" name="codice fiscale" placeholder="digita codice fiscale" className="form-control Dim" />
+                    </div>
 
-                <div>
-                    <input type="text" name="codice fiscale" placeholder="digita codice fiscale" className="form-control Dim" />
-                </div>
+                    <div>
+                        <input type="text" name="iban" placeholder="digita iban" className="form-control Dim" />
+                    </div>
 
-                <div>
-                    <input type="text" name="iban" placeholder="digita iban" className="form-control Dim" />
-                </div>
+                    <div>
+                        <input type="email" name="email" placeholder="digita email" id="emailRegistrazione" className="form-control Dim" />
+                    </div>
 
-                <div>
-                    <input type="email" name="email" placeholder="digita email" id="emailRegistrazione" className="form-control Dim" />
-                </div>
+                    <div>
+                        <input type="text" name="telefono" placeholder="digita telefono" className="form-control Dim" />
+                    </div>
+                    <div>
+                        <input type="text" name="indirizzo" placeholder="digita indirizzo" className="form-control Dim" />
+                    </div>
+                    <div>
+                        <input type="password" placeholder="digita password" id="pswRegistrazione" className="form-control Dim" />
+                    </div>
 
-                <div>
-                    <input type="text" name="telefono" placeholder="digita telefono" className="form-control Dim" />
-                </div>
-                <div>
-                    <input type="text" name="indirizzo" placeholder="digita indirizzo" className="form-control Dim" />
-                </div>
-                <div>
-                    <input type="password" placeholder="digita password" id="pswRegistrazione" className="form-control Dim" />
-                </div>
+                    <button type="submit" className="btn btn-outline-primary" onClick={() => submit()}>registrati</button>
 
-                <button type="submit" className="btn btn-outline-primary">registrati</button>
-
-            </form>
-
+                </form>
+            </div>
 
         </>
 
