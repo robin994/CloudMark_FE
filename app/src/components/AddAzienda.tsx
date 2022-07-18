f8149cfc83725b784af86b8955826f71533076fa
Lorenzo <lamu1197@hotmail.com>
2022-07-18 14:56:57 +0200

    EDIT :Add Azienda

--- app/src/components/AddAzienda.tsx
+++ app/src/components/AddAzienda.tsx
@@ -1,32 +1,56 @@
 import { useState } from "react";
 import './css_components/addAzienda.css'
+import Card from "react-bootstrap/Card"
+import Container from "react-bootstrap/Container"
+import Spacer from "./Spacer"
 
 const AddAziendaComponent=()=>{
-  const [inv, setInv] = useState(true)
-  function okPop(){
-    if(inv == true){
-      setInv(false)
-    }
-  }
+ 
+ 
   return(
     <>
-      <div className='cont_01 mt-5'>
-        <div id='main_body'>
-          <h3>Aggiungi Azienda</h3>
-          <input type="text" className="form-control" placeholder='nome'/>
-          <input type="text" className="form-control" placeholder='p_iva'/>
-          <input type="text" className="form-control" placeholder='iban'/>
-          <input type="text" className="form-control" placeholder='indirizzo'/>
-          <input type="text" className="form-control" placeholder='telefono'/>
-          <input type="text" className="form-control" placeholder='email'/>
-        </div>
-      </div>
-      <div id="alert_back">
-        <div id="alert_container" hidden={inv}>
-          <h1 onClick={okPop}>Hai sbagliato</h1>
-          <button className="btn btn-primary">Ok</button>
-        </div>
-      </div>
+
+<Spacer margin="20px" />
+            <Container>
+                <Card style={{ width: '24rem' }} className="mx-auto">
+                    <Card.Header className="text-center">
+                        <h1>Aggiungi Azienda</h1>
+                    </Card.Header>
+                    <Card.Body>
+
+                        <form action="">
+                   
+                            <div>
+                                <input type="text" placeholder="Nome" className="form-control my-3" required/>
+                            </div>
+
+                            <div>
+                                <input type="text"  placeholder="Partita Iva" className="form-control my-3" required/>
+                            </div>
+                            
+                            <div>
+                                <input type="text" placeholder="iban"  className="form-control my-3" required/>
+                            </div>
+
+                            <div>
+                                <input type="email" name="email" placeholder="Email" id="emailRegistrazione" className="form-control my-3" required/>
+                            </div>
+
+                            <div>
+                                <input type="tel" name="telefono" placeholder="Telefono" className="form-control my-3" required/>
+                            </div>
+                            <div>
+                                <input type="text" name="indirizzo" placeholder="Indirizzo" className="form-control my-3" required />
+                            </div>
+                           
+
+                            <button type="submit" className="btn btn-outline-primary">Conferma </button>
+
+                        </form>
+                    </Card.Body>
+                </Card>
+            </Container>
+ 
     </>
   )
 }
