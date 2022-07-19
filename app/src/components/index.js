9f3cd6b32373fb701e91b817b434355c22b69627
Lorenzo <lamu1197@hotmail.com>
2022-07-19 16:34:54 +0200

    Edit: inde js

--- app/src/components/index.js
+++ app/src/components/index.js
@@ -10,7 +10,7 @@ import ModAziendaAdmin  from "./ModAziendaAdmin"
 import addAziendaComponent from "./AddAzienda"
 // New separe index should be created for root pages
 import ListaDipendenti from "../pages/dipendenti/ListaDipendenti"
-import Tab from './Table'
+import TableDipendenti from './TableDipendenti'
 
 export { CercaDipendente, Login, Registrazione, TopMenu, SuperUser, Dashboard, DipendentiLista,
-         Dipendente, ModAziendaAdmin, addAziendaComponent, ListaDipendenti, Tab }
+         Dipendente, ModAziendaAdmin, addAziendaComponent, ListaDipendenti, TableDipendenti }
