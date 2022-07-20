bdb228008d805d5b71b0fa9d3c6dc782faa4d305
Alessandro Rocco <107470489+skaysrei@users.noreply.github.com>
2022-07-20 14:54:59 +0200

    Clienti Update

--- app/src/pages/admin/clienti.tsx
+++ app/src/pages/admin/clienti.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import { Container, Row, Col } from "react-bootstrap"
+import { useNavigate } from "react-router-dom"
+import { getListaDipendenti } from "../../data_mock"
+import CustomCard from "../../components/CustomCard"
+import { useEffect } from "react"
+import Spacer from "../../components/Spacer"
 
-export default function clienti() {
-  return (
-    <h1>Clienti</h1>
-
-  )
+export default function Clienti() {
+    const navigate = useNavigate()
+    useEffect(() => {
+        if (sessionStorage.auth === undefined)
+            navigate("/login", {replace: true})
+    }, [navigate])
+    let lista_dipendenti = getListaDipendenti()
+    let comps_Dipendente = []
+    for (const dip of Object.values(lista_dipendenti)) {
+        comps_Dipendente.push(dip)
+    }
+    return (
+        <>
+            <Container>
+                <Spacer margin='80px' />
+                <Container>
+                <Row>
+                    <Col>
+                    <h1>
+                        {`${sessionStorage.azienda_nome} Dipendenti`}
+                    </h1>
+                    </Col>
+                </Row>
+                <Row>
+                {comps_Dipendente.map(dip => (
+                    <Col>
+                        <CustomCard cardTitle={`${dip.nome} ${dip.cognome}`}
+                        imgSrc='https://www.svgrepo.com/show/12496/users.svg'
+                        navPath={`/dipendente/${dip.id_employee}`}
+                        buttonText='Dettagli'
+                        textDesc={dip.telefono}/>
+                    </Col>
+                ))}
+                </Row>
+                </Container>
+                <Spacer margin='80px' />
+            </Container>
+        </>
+    )
 }
