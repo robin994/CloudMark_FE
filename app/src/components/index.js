33d7cc9f86fc239cae42d00bc6db6acbda5fc6ae
Alessandro Rocco <107470489+skaysrei@users.noreply.github.com>
2022-07-20 12:00:38 +0200

    Updated Routes

--- app/src/components/index.js
+++ app/src/components/index.js
@@ -1,15 +1,16 @@
+import Dashboard from "../pages/admin/dashboard"
+
 import CercaDipendente from "./CercaDipendente"
 import Login from "./Login"
 import Registrazione from "./Registrazione"
 import TopMenu from "../pages/navbar"
 import SuperUser from "./Super_User"
-import Dashboard from "../pages/employee/dashboard"
 import DipendentiLista from "./DipendentiLista"
 import Dipendente from "./Dipendente"
 import ModAziendaAdmin  from "./ModAziendaAdmin"
 import addAziendaComponent from "./AddAzienda"
 // New separe index should be created for root pages
-import ListaDipendenti from "../pages/employee/dipendenti"
+import ListaDipendenti from "../pages/admin/dipendenti"
 import TableDipendenti from './TableDipendenti'
 
 export { CercaDipendente, Login, Registrazione, TopMenu, SuperUser, Dashboard, DipendentiLista,
