9a7692c22e073ee65fad560fa1f721605c6e1b7c
JJJJJJack <97jack@live.it>
2022-07-20 11:29:47 +0200

    REFACTOR: empty strings, hooks must be on top, arrow functions with immediate return value

--- app/src/components/Super_User.jsx
+++ app/src/components/Super_User.jsx
@@ -8,24 +8,18 @@ import Spacer from "./Spacer";
 import './css_components/SuperUser.css'
 import { getAziendaData } from './data_mock';
 
-
-
-
-
-
-const SuperUser = () => {
-  
+export default function SuperUser() {
     let data = getAziendaData()
     const [filteredData, setFilteredData] = useState([]);
     const [wordEntered, setWordEntered] = useState("");
-
+    const [showDelete, setShowDelete] = useState(false);
+    const [show, setShow] = useState(false);
     const Filter = (event) => {
         const searchWord = event.target.value;
         setWordEntered(searchWord);
         const newFilter = data.filter((value) => {
           return value.azienda.toLowerCase().includes(searchWord.toLowerCase());
         });
-    
         if (searchWord === "") {
           setFilteredData([]);
         } else {
@@ -33,16 +27,11 @@ const SuperUser = () => {
         }
       };
 
- 
-    const [show, setShow] = useState(false);
-
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-
-    const [showDelete, setShowDelete] = useState(false);
-
     const handleCloseDelete = () => setShowDelete(false);
     const handleShowDelete = () => setShowDelete(true);
+
     return (
     <>
     <Spacer margin="20px"/>
@@ -60,116 +49,76 @@ const SuperUser = () => {
           value={wordEntered}
           onChange={Filter}
         />
-      
       </div>
       {filteredData.length != 0 && (
         <div className="dataResult">
-          {filteredData.slice(0, 15).map((value, key) => {
-            return (
-              
-                <p className="azienda_p">{value.azienda} </p>
-              
-            );
-          })}
+          {filteredData.slice(0, 15).map(
+            (value, key) => <p className="azienda_p">{value.azienda} </p>
+          )}
         </div>
       )}
     </div>
-
-
-
-
-
-
-
     <div className="mybox">
-  
-       
         <div>
         <h2 className="modify_azienda">Modifica Azienda</h2>
         <h2 hidden>Super User</h2>
         <form action="">
             <p>
-                    
-                    <input type="text"  placeholder="id_azienda"  className="form-control my-1"/>
-                    <input type="text"  placeholder="p_iva" className="form-control my-1" />
-                    <input type="text"  placeholder="iban"  className="form-control my-1"/>
-                    <input type="text"  placeholder="indirizzo" className="form-control my-1"/>
-                    <input type="tel"  placeholder="telefono"  className="form-control my-1"/>
-                    <input type="email"  placeholder="email" className="form-control my-1"/>
-                    <input type="email"  placeholder="pec" className="form-control my-1"/>
-                    <input type="tel" placeholder="fax" className="form-control my-1"/>
-               </p>
-                
-                <p>
-                <Button variant="primary" onClick={handleShow}>
-                    Conferma Modifica
-                </Button>
-                
-                
-                <Modal show={show} onHide={handleClose}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Modifica Azienda</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        Sicuro di modificare l'Azienda?
-                   
-                    </Modal.Body>
-                    <Modal.Footer>
-                    <Button variant="success" onClick={handleClose}>
-                            Sì
-                        </Button>
-                        <Button variant="danger" onClick={handleClose}>
-                            No
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
-
-
-                <Button variant="danger" onClick={handleShowDelete}>
-                    Elimina Azienda
-                </Button>
-                <Modal show={showDelete} onHide={handleCloseDelete}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Elimina Azienda</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        Sicuro di Eliminare l'Azienda?
-                   
-                    </Modal.Body>
-                    <Modal.Footer>
-                    <Button variant="success" onClick={handleCloseDelete}>
-                            Sì
-                        </Button>
-                        <Button variant="danger" onClick={handleCloseDelete}>
-                            No
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
-                </p>
-
-                
+              <input type="text"  placeholder="id_azienda"  className="form-control my-1"/>
+              <input type="text"  placeholder="p_iva" className="form-control my-1" />
+              <input type="text"  placeholder="iban"  className="form-control my-1"/>
+              <input type="text"  placeholder="indirizzo" className="form-control my-1"/>
+              <input type="tel"  placeholder="telefono"  className="form-control my-1"/>
+              <input type="email"  placeholder="email" className="form-control my-1"/>
+              <input type="email"  placeholder="pec" className="form-control my-1"/>
+              <input type="tel" placeholder="fax" className="form-control my-1"/>
+            </p>
+            <p>
+            <Button variant="primary" onClick={handleShow}>
+                Conferma Modifica
+            </Button>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modifica Azienda</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Sicuro di modificare l'Azienda?
+                </Modal.Body>
+                <Modal.Footer>
+                <Button variant="success" onClick={handleClose}>
+                        Sì
+                    </Button>
+                    <Button variant="danger" onClick={handleClose}>
+                        No
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+            <Button variant="danger" onClick={handleShowDelete}>
+                Elimina Azienda
+            </Button>
+            <Modal show={showDelete} onHide={handleCloseDelete}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Elimina Azienda</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Sicuro di Eliminare l'Azienda?
+                </Modal.Body>
+                <Modal.Footer>
+                <Button variant="success" onClick={handleCloseDelete}>
+                        Sì
+                    </Button>
+                    <Button variant="danger" onClick={handleCloseDelete}>
+                        No
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+            </p>
             </form>
-
-
-            
         </div>
     </div>
       </Card.Body>
       </Card>
     </Container>
-
-
-
-
-    
     </>
   )
 }
-
-
-
-
-
-
-
-export default SuperUser
