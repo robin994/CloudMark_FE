8c300b72d51e4c76273f29ea5027e3b189275674
Lorenzo <lorprofessional@gmail.com>
2022-07-21 16:07:37 +0200

    mod_bella

--- app/src/components/AddAzienda.tsx
+++ app/src/components/AddAzienda.tsx
@@ -4,42 +4,68 @@ import Container from "react-bootstrap/Container"
 import Spacer from "./Spacer"
 
 const AddAziendaComponent=()=>{
- 
-    return(
-        <>
-            <Spacer margin="20px" />
-                <Container>
-                    <Card style={{ width: '24rem' }} className="mx-auto">
-                        <Card.Header className="text-center">
-                            <h1>Modifica Azienda</h1>
-                        </Card.Header>
-                        <Card.Body>
-                            <form action="">
-                                <div>
-                                    <input type="text" placeholder="Nome" className="form-control my-3" required/>
-                                </div>
-                                <div>
-                                    <input type="text"  placeholder="Partita Iva" className="form-control my-3" required/>
-                                </div>
-                                <div>
-                                    <input type="text" placeholder="iban"  className="form-control my-3" required/>
-                                </div>
-                                <div>
-                                    <input type="email" name="email" placeholder="Email" id="emailRegistrazione" className="form-control my-3" required/>
-                                </div>
-                                <div>
-                                    <input type="tel" name="telefono" placeholder="Telefono" className="form-control my-3" required/>
-                                </div>
-                                <div>
-                                    <input type="text" name="indirizzo" placeholder="Indirizzo" className="form-control my-3" required />
-                                </div>
-                                <button type="submit" className="btn btn-outline-primary">Conferma Modifica</button>
-                            </form>
-                        </Card.Body>
-                    </Card>
-                </Container>
-        </>
-    )
+
+  return(
+    <>
+        <Spacer margin="20px" />
+            <Container>
+                <Card style={{ width: '24rem' }} className="mx-auto">
+                    <Card.Header className="text-center">
+                        <h1>Aggiungi Azienda</h1>
+                    </Card.Header>
+                    <Card.Body>
+
+                        <form action="submit">
+                    
+                            <div>
+                                <input type="text" placeholder="Nome" className="form-control my-3" required/>
+                            </div>
+
+                            <div>
+                                <input type="text"  placeholder="Partita Iva" className="form-control my-3" required/>
+                            </div>
+                            
+                            <div>
+                                <input type="text" placeholder="Iban"  className="form-control my-3" required/>
+                            </div>
+
+                            <div>
+                                <input type="email" name="email" placeholder="Email" id="emailReg" className="form-control my-3" required/>
+                            </div>
+
+                            <div>
+                                <input type="email" name="pec" placeholder="Pec" id="pecReg" className="form-control my-3" required/>
+                            </div>
+
+                            <div>
+                                <input type="tel" name="telefono" placeholder="Telefono" className="form-control my-3" required/>
+                            </div>
+
+                            <div>
+                                <input type="text" name="indirizzo" placeholder="Indirizzo" className="form-control my-3" required />
+                            </div>
+
+                            <div>
+                                <input type="text" name="cap" placeholder="Cap" className="form-control my-3" required/>
+                            </div>
+
+                            <div>
+                                <input type="text" name="fax" placeholder="Fax" className="form-control my-3" required/>
+                            </div>
+                            
+
+                            
+
+                            <button type="submit" className="btn btn-outline-primary my-2">Conferma Modifica</button>
+
+                        </form>
+                    </Card.Body>
+                </Card>
+            </Container>
+
+    </>
+  )
+
 }
 
 export default AddAziendaComponent
--- app/src/components/ModAziendaAdmin.tsx
+++ app/src/components/ModAziendaAdmin.tsx
@@ -1,6 +1,8 @@
-import React, {useState} from "react";
+import React, {useState, Component} from "react";
 import './css_components/ModAziendaAdmin.css'
 import datafake from './data_mock.json'
+import axios from "axios";
+
 
 
 
@@ -9,6 +11,8 @@ function ModAziendaAdmin() {
   const [pop, setPop] = useState(true)
   const [error, setError] = useState("")
   const [countErr, setCountErr] = useState(0)
+  const [data, setData] = useState()
+  const[controlledData, setControlledData] = useState()
   function controlInfo(){
     if(oggetto[0].nome !== undefined && oggetto[1].p_iva !== undefined && oggetto[2].iban !== undefined && oggetto[3].indirizzo !== undefined && oggetto[4].cap !== undefined && oggetto[5].telefono !== undefined && oggetto[6].email !== undefined && oggetto[7] !== undefined && oggetto[8].fax !== undefined){
       if(oggetto[2].iban.trim().length < 15){
@@ -20,12 +24,34 @@ function ModAziendaAdmin() {
         setError("ci sono " +countErr+ " errori")
       }
     }
+
   }
+  const getAziendaData = async ()=>{
+    try{let sus = await axios({
+      url:"http://127.0.0.1:8000/business",
+      method:"get",
+      timeout:8000,
+      headers:{
+        "content-type": "json"
+      }
+    })
+    if(sus.status=200){
+      console.log(sus.status)
+    }
+    return sus.data
+    }catch(err){
+      console.log(err)
+    }
+  }
+
+  getAziendaData()
+
   return(
     <>
       <div className='cont_01 mt-5'>
         <div id="azienda_temp">
           <h3>Info Azienda</h3>
+          <div>{}</div>
           <div className="form-control">{datafake.nome_azienda}</div>
           <div className="form-control">{datafake.p_iva_azienda}</div>
           <div className="form-control">{datafake.iban_azienda}</div>
@@ -42,11 +68,11 @@ function ModAziendaAdmin() {
           <input type="text" className="form-control" placeholder='p_iva' id="p_iva" required onChange={(val)=>{setOggetto([{email:val.target.value}])}}/>
           <input type="text" className="form-control" placeholder='iban' id="iban" required onChange={(val)=>{setOggetto([{iban:val.target.value}])}}/>
           <input type="text" className="form-control" placeholder="cap" id="cap" required onChange={(val)=>{setOggetto([{cap:val.target.value}])}}/>
-          <input type="text" className="form-control" placeholder='indirizzo' id="indirizzo" onChange={(val)=>{setOggetto([{indirizzo:val.target.value}])}}/>
+          <input type="text" className="form-control" placeholder='indirizzo' id="indirizzo" required onChange={(val)=>{setOggetto([{indirizzo:val.target.value}])}}/>
           <input type="text" className="form-control" placeholder='telefono' id="telefono" required onChange={(val)=>{setOggetto([{telefono:val.target.value}])}}/>
           <input type="email" className="form-control" placeholder='email' id="email" required onChange={(val)=>{setOggetto([{email:val.target.value}])}}/>
-          <input type="email" className="form-control" placeholder='pec' id="pec" onChange={(val)=>{setOggetto([{pec:val.target.value}])}}/>
-          <input type="text" className="form-control" placeholder="fax" id="fax" onChange={(val)=>{setOggetto([{fax:val.target.value}])}}/>
+          <input type="email" className="form-control" placeholder='pec' id="pec" required onChange={(val)=>{setOggetto([{pec:val.target.value}])}}/>
+          <input type="text" className="form-control" placeholder="fax" id="fax" required onChange={(val)=>{setOggetto([{fax:val.target.value}])}}/>
           <button className="btn btn-primary" onClick={controlInfo}>Modifica Azienda</button>
           <span className="alert alert-danger mt-3" hidden={pop}>{error}</span>
         </div>
