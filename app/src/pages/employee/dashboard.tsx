c31a4f8698587294a37481427c998c91ecaf4d14
Alessandro Rocco <107470489+skaysrei@users.noreply.github.com>
2022-07-21 16:58:25 +0200

    Employee Dashboard

--- app/src/pages/employee/dashboard.tsx
+++ app/src/pages/employee/dashboard.tsx
@@ -1,5 +1,15 @@
+import DataTable from "../../components/DataTable"
+import { getMockPresenzeHeading, getMockPresenzeDate } from "../../data_mock.js"
+
 export default function Dashboard() {
+
+  const tableWidget = (
+    <DataTable id='date' col={getMockPresenzeHeading()} rows={getMockPresenzeDate()} setInputField={()=> false}/>
+  )
+
   return (
-    <div>dashboard</div>
+    <>
+      {tableWidget}
+    </>
   )
 }
