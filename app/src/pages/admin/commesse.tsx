326b3fb54d4a9bcc5302918e5fd90a617115b2eb
JJJJJJack <97jack@live.it>
2022-07-24 19:07:20 +0200

    ADD: commesse component

--- app/src/pages/admin/commesse.tsx
+++ app/src/pages/admin/commesse.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DataTable from '../../components/DataTable'
+import Axios from 'axios'
+
+const heading = {
+  id_order: "ID commessa",
+  id_customer: "ID cliente",
+  id_business: "ID azienda",
+  description: "Descrizione",
+  startDate: "Data inizio",
+  endDate: "Data fine"
+}
 
 export default function Commesse() {
+  const [commesse, setCommesse] = useState([])
+  useEffect( () => {
+    Axios("http://localhost:8000/orders").then( resp => {
+      const data = resp.data.data
+      setCommesse(Object.values(data))
+  }).catch( err => { throw err })
+  }, [])
   return (
-    <h1>Commesse</h1>
+    <DataTable id="id_order" col={heading} rows={commesse} />
   )
 }
