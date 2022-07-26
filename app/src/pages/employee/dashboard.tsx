f47ce00127206504f3a5c7b8c9679338dddd2c13
Dev1 - rossi <94alex.rossi@gmail.com>
2022-07-26 15:41:37 +0200

    fixed-calendar

--- app/src/pages/employee/dashboard.tsx
+++ app/src/pages/employee/dashboard.tsx
@@ -1,22 +1,32 @@
 import Container from "react-bootstrap/Container"
 import{ Row, Col } from "react-bootstrap"
-import MyCalendar from "./myCalendar"
+// import MyCalendar from "./myCalendar"
 import Commesse from "./commesse"
 import Presenze from "./components/presenze"
+import CalendarMY from "./calender"
 
 export default function Dashboard() {
 
   const presenzeWidget = (
     <Presenze />
   )
+  const getDate = (month: any,year:any)=>{
+    console.log(`month:${month+1} year : ${year}`)
+  }
 
   const calendarWidget = (
-    <MyCalendar />
+    <CalendarMY 
+    callbackData = {getDate}
+    />
   )
 
   const commesseWidget = (
     <Commesse />
   )
+
+
+
+
 
   return (
     <>
