54181be341387df5c89ae2053a8c51e3fcf6645e
DEV1 - Alessandro <alerocco90@gmail.com>
2022-07-26 11:05:51 +0200

    DataTable Updates

--- app/src/pages/employee/components/presenze.tsx
+++ app/src/pages/employee/components/presenze.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
 import DataTable from "../../../components/DataTable"
 
 // Mock imports (For development purpouses)
 import { getMockPresenzeHeading, getMockPresenzeDate } from "../../../data_mock.js"
 
+function callAlert(msg: string) {
+  alert(msg)
+}
 export default function Presenze() {
   return (
-    <DataTable id='date' col={getMockPresenzeHeading()} rows={getMockPresenzeDate()} />
+    <DataTable id='date' col={getMockPresenzeHeading()} rows={getMockPresenzeDate()} btnCallback={callAlert} />
   )
 }
