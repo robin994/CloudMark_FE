dc7632a168caf3b6cd4cb071e3c202bb9dc29f31
Roberto <robin994@hotmail.it>
2022-07-28 16:30:56 +0200

    fix data session

--- app/src/components/PaginaProfilo.tsx
+++ app/src/components/PaginaProfilo.tsx
@@ -18,6 +18,17 @@ interface InterfacciaUtente {
 
 }
 
+interface SessionInterface {
+
+    id_account: string,
+    abilitate: string,
+    accountType: string,
+    accountTypeName: string,
+    accountListFunction: string,
+    user: string
+
+}
+
 export default function ProfiloUtente() {
 
     return (
@@ -53,10 +64,12 @@ export default function ProfiloUtente() {
 }
 
 function FilterData(prop: InterfacciaToken) {
-    const [data, setData] = useState()
+    const [data, setData] = useState<SessionInterface>()
     const [filtredData, setFiltredData] = useState([])
     function ChiamaUtente() {
-        setData(JSON.parse(JSON.stringify(jwt_decode(sessionStorage.bearer))))
+        setData(jwt_decode(sessionStorage.bearer));
+        
+        console.log(data?.id_account);
     }
     return (
         <button className="btn btn-primary mt-5" onClick={ChiamaUtente}>Salva Modifiche</button>
