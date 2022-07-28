4a03b9a6321653c785fd50599cee56f71de5e458
Dev1 - rossi <94alex.rossi@gmail.com>
2022-07-28 17:38:17 +0200

    added-post-to-finish

--- app/src/pages/admin/components/TabellaPresenze.tsx
+++ app/src/pages/admin/components/TabellaPresenze.tsx
@@ -5,30 +5,73 @@ import axios from 'axios'
 export default function TabellaPresenze() {
     const [reveal, setReveal] = useState(true)
     const [presenze, setPresenze] = useState([])
+    const [inputPresenze, setInputPresenze] = useState([])
+    const[date_presence, setDatePresence] = useState()
+    const[tipo_presenza, setTipoPresenza] = useState()
+    const[hours, setHours] = useState()
 
+    const arr: any = []
+    let typeInput = "text"
     async function getPresenze() {
         try {
             const response = await axios.get(`${process.env.REACT_APP_FASTAPI_URL}/presence/all/first_name/last_name/`)
             setPresenze(response.data.data)
-        } catch(error) {
+        } catch (error) {
             throw error
         }
     }
 
-    useEffect(()=> {
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        axios.post(`${process.env.REACT_APP_FASTAPI_URL}/presence/insertUpdate`, {
+            inputPresenze:[{
+            idDipendente : presenze,
+            dataPresenza : date_presence,
+            tipoPresenza : tipo_presenza,
+            idCommessa : presenze,
+            ore : hours
+        }]
+
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+
+    useEffect(() => {
         getPresenze()
     }, [])
 
+    presenze.map((el, index) => {
+        return arr.push(Object.keys(el)[index])
+    })
+    console.log(arr)
+
+
     const test = presenze.map((el, index) => {
+        console.log(Object.keys(el)[index])
+
         return (
             <tr ng-repeat="name in getdrugnameNewArray" key={index}>
                 <td><input type="text" placeholder="prova" /></td>
                 {
                     Object.values(el).map((e: any, i) => {
+                        // console.log()
+                        // if (Object.keys(el)[index] === "hours") {
+                        //     typeInput = "number"
+                        // }
+                        // if (Object.keys(el)[index] === "hours") {
+                        //     typeInput = "date"
+                        // }
+
                         return (
                             <td key={i}>
                                 <div className='input-group mb-3'>
-                                    <input type="text" className='form-control' placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" value={e} />
+                                    <input type={typeInput} className='form-control' placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" value={e} />
                                 </div>
                             </td>
                         )
@@ -55,13 +98,13 @@ export default function TabellaPresenze() {
     })
 
     let change = () => {
-        if (reveal == true) {
+        if (reveal === true) {
             setReveal(false)
         } else {
             setReveal(true)
         }
     }
-
+    console.log(presenze)
     return (
         <>
             <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
@@ -70,18 +113,18 @@ export default function TabellaPresenze() {
                 <thead>
                     <tr>
                         <th className="col-md-auto text-center custom-head">N#</th>
-                            {
-                                presenze.map((e: any, i: any) => {
-                                    return (
-                                        <th key={i} className='cold-md-auto text-center custom-head'>{Object.keys(e)[i]}</th>
-                                    )
-                                })
-                            }
+                        {
+                            presenze.map((e: any, i: any) => {
+                                return (
+                                    <th key={i} className='cold-md-auto text-center custom-head'>{Object.keys(e)[i]}</th>
+                                )
+                            })
+                        }
                     </tr>
                 </thead>
                 <tbody>
-                    {    
-                        reveal == true ? test2 : test  
+                    {
+                        reveal === true ? test2 : test
                     }
                 </tbody>
             </table>
