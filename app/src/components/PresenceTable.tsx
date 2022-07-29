615acd680837bdd0df2f5123b417518393bc4c4a
Alessandro Rocco <107470489+skaysrei@users.noreply.github.com>
2022-07-29 12:10:44 +0200

    Updates on Employee/Presences

--- app/src/components/PresenceTable.tsx
+++ app/src/components/PresenceTable.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
-import { Button, Card, Form, Table } from 'react-bootstrap'
+import { Button, Card, Form, Table, Dropdown } from 'react-bootstrap'
 
 interface DataTableProps {
     id: string,
@@ -19,42 +19,35 @@ interface Presenza {
 }
 
 const heading = {
+    position: '#',
     date_presence: "Data",
     hours: "Ore",
-    standard: "Standard",
-    festivo: "Festivo",
-    malattia: "Malattia",
-    assenza: "Assenza",
+    type: "Tipo Presenza",
     id_presence: "ID presenza"
+}
+
+const types: any = {
+    'ca34d37e-600c-452e-a8e4-2efb53161812': 'Standard',
+    '6dc55260-7150-4f76-8251-adc4c3fc15b4': 'Assenza',
+    'a8fd713d-36e8-440f-81e1-6e7314a3c417': 'Festivo',
+    'b867b283-38a0-4eb3-8df1-55ccb5f310df': 'Malattia'
 }
 
 export default function EditableTable(props: DataTableProps) {
     const [rows, setRows] =  useState(props.rows);
-    const [presenceTypes, setPresenceTypes] = useState([]);
     const [presenceChecked, setPresenceChecked] = useState([]);
-
-    async function getPresenceTypes() {
-        try {
-            const response = await axios.get(`${process.env.REACT_APP_FASTAPI_URL}/type/presence`);
-            console.log('TIPI PRESENZE', response.data.data);
-            setPresenceTypes(response.data.data);
-        } catch(e) {
-            console.log(e)
-        }
-    }
-
-    useEffect(()=> {
-        getPresenceTypes();
-    }, [])
 
     const DataColumn = ({element}: {element: string})=> {
         return element === heading['id_presence'] && !props.showID ? <></> : <>{element}</>
     }
-    const DataRow = ({element}: {element: DynamicObject})=> {
+    const DataRow = ({element, key}: any)=> {
         const isChecked = (id: string) => id === element['id_tipoPresenza'] ? true : false
-
+        console.log(key)
         return(
             <>
+                <td>
+                    
+                </td>
                 <td>
                     <Form.Group>
                         <Form.Control type="date" value={element.date_presence} onChange={()=> false} />
@@ -65,7 +58,21 @@ export default function EditableTable(props: DataTableProps) {
                         <Form.Control type="number" value={element.hours} onChange={()=> false} />
                     </Form.Group>
                 </td>
-                {presenceTypes.map((element: any, key: number)=> {
+                <td>
+                <Dropdown>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        {types[element['id_tipoPresenza']]}
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        {Object.values(types).map((element: any)=> {
+                            return(
+                                <Dropdown.Item href="#/action-1">{element}</Dropdown.Item>
+                            )
+                        } )}
+                    </Dropdown.Menu>
+                </Dropdown>
+                </td>
+                {/* {presenceTypes.map((element: any, key: number)=> {
                         return(
                             <td>
                                 <Form.Group>
@@ -81,10 +88,8 @@ export default function EditableTable(props: DataTableProps) {
                             </td>
                         )}
                     )
-                }
-                <td>
-                    {props.showID && element[`${'id_presence'}`]}
-                </td>
+                } */}
+                {props.showID && <td>element[`${'id_presence'}`]</td>}
             </>
         )
     }
@@ -92,21 +97,21 @@ export default function EditableTable(props: DataTableProps) {
     // This return needs to be cleaned up for readability
     return (
         <>
-            <Card className="vh-100">
+            <Card>
                 <Card.Body>
-                        <Table striped hover responsive>
+                        <Table striped bordered hover responsive className="vh-100">
                             <thead>
                                 <tr>
                                     {Object.values(heading).map((element: string)=> <React.Fragment key={`col-${element}`}><th><DataColumn element={element} /></th></React.Fragment>)}
                                 </tr>
                             </thead>
                             <tbody>
-                                {props.rows.map((element: { [key: string]: any })=> {
+                                {props.rows.map((element: any, key: number)=> {
                                     return(
                                         <>
                                             <React.Fragment key={`item-${element[props.id]}`}>
                                                     <tr>
-                                                        <DataRow element={element} />
+                                                        <DataRow element={element} key={key} />
                                                         {props.btnCallback && <td><Button onClick={()=> props.btnCallback?.(element[props.id])} variant="outline-primary">Modifica</Button></td>}
                                                     </tr>
                                             </React.Fragment>
