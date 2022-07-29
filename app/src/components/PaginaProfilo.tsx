6877412fe8c66b161f1773bb9687d293b333abdf
Lorenzo <lorprofessional@gmail.com>
2022-07-29 16:00:06 +0200

    smallChangesErr

--- app/src/components/PaginaProfilo.tsx
+++ app/src/components/PaginaProfilo.tsx
@@ -13,11 +13,24 @@ interface SessionInterface {
     user: string
 }
 
+// interface EmployeesAccount{
+//     "124e4567-e85b-1fd3-a456-333322233412":{
+//         first_name: string,
+//         last_name: string,
+//         cf: string,
+//         iban: number,
+//         id_contractType: string,
+//         email: string,
+//         phoneNumber: number, 
+//         id_employee: string
+//     }
+// }
+
 
 
 export default function ProfiloUtente() {
     const [data, setData] = useState<SessionInterface>()
-    const [filtredData, setFiltredData] = useState([''])
+    const [filtredData, setFiltredData] = useState([])
     const [filtredAccount, setFiltredAccount] = useState([])
     const [hid, setHid] = useState(true)
     const [btnMsg, setBtnMsg] = useState('Modifica')
@@ -27,20 +40,33 @@ export default function ProfiloUtente() {
     const [CF, setCF] = useState('')
     const [iban, setIban] = useState('')
     const [tel, setTel] = useState('')
+    const [dataProva, setDataProva] = useState('')
 
     function getAccount(){
         if(data !== undefined){
             axios.get(`${process.env.REACT_APP_FASTAPI_URL}/employee/account/${data.id_account}`)
             .then(res=>{
                 console.log(res.data.data)
-                const dat = res.data.data
-                setFiltredData(dat)
+                setFiltredData(res.data.data)
             })
             .catch(err=>{console.log(err)})
         }
-
+    }
+    function FiltroBello(){
+        return(
+            <>
+                {filtredData.map(res=>{
+                    return(
+                        <div>
+                            <p>{res}</p>
+                        </div>
+                    )
+                })}
+            </>
+        )
     }
     function FilterData() {
+        
         function ChiamaUtente() {
             if(hid === true){
                 setHid(false)
@@ -91,7 +117,7 @@ export default function ProfiloUtente() {
                         <h2>Numero Di Telefono:</h2>
                         <input type="text" disabled={hid} onChange={val=> setTel(val.target.value)} required/>
                     </div>
-                    {<FilterData/>}
+                    {<FiltroBello/>}
                 </div>
             </div>
         </>
