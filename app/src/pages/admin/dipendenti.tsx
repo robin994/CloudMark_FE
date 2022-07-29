504c5f23e13c0e7d9756dd2efe248de5f7631ab7
Lorenzo <lamu1197@hotmail.com>
2022-07-29 16:55:49 +0200

    add create dipendente

--- app/src/pages/admin/dipendenti.tsx
+++ app/src/pages/admin/dipendenti.tsx
@@ -5,13 +5,36 @@ import { motion } from 'framer-motion';
 
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import './components/css_components/TabellaDipendenti.css'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
 
 
 
 const ListaDipendenti=(props:any)=> {
 
+
+const [createdipendenti, setCreateDipendenti] = useState({
+  
+  first_name:'',
+  last_name:'',
+  cf: '',
+  iban: '',
+  id_contractType:'' ,
+  email: '',
+  phoneNumber: '',
+  
+})
+
   
   const [dipendenti, setDipendenti] = useState([])
+ 
+
+
+
+  const [show, setShow] = useState(false)
+
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
 
 
   useEffect( () => {
@@ -21,6 +44,35 @@ const ListaDipendenti=(props:any)=> {
   }).catch( err => { throw err })
   },[])
   console.log(dipendenti)
+  
+  
+  function submit(e:any) {
+      e.preventDefault();
+      axios.post(`${process.env.REACT_APP_FASTAPI_URL}/employee/create/`,{
+        first_name:createdipendenti.first_name,
+        last_name:createdipendenti.last_name,
+        cf:createdipendenti.cf,
+        iban:createdipendenti.iban,
+        id_contractType:createdipendenti.id_contractType,
+        email:createdipendenti.email,
+        phoneNumber:createdipendenti.phoneNumber,
+        
+      })
+      .then(res=>{
+        console.log(res.data)
+      })
+    }
+   
+
+    function handle(e:any){
+      const newdata={...createdipendenti}
+      newdata[e.target.id] = e.target.value
+      setCreateDipendenti(newdata)
+      console.log(newdata)
+    }
+
+  
+
 
 
   let list = dipendenti.map(el => {
@@ -53,7 +105,39 @@ const columns: GridColDef[] = [
   return (
 
 
+<>
+<div style={{marginTop:"2vh",backgroundColor:"#ffffff",borderBottom:"1px solid black",borderTop:"1px solid black",padding:"0.5rem",marginBottom:"2vh"}}>
 
+  <button className="btn btn-primary"  style={{marginTop:"2vh"}}>Salva</button>
+  <Button variant="secondary" onClick={handleShow}  style={{marginTop:"2vh",marginLeft:"10px"}}>
+                + Aggiungi Dipendente
+            </Button>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Aggiungi Dipendente</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <form onSubmit={(e)=>submit(e)}>
+                  <input onChange={(e)=>handle(e)} id="first_name" value={createdipendenti.first_name}className="form-control" placeholder="nome"  style={{marginTop:"1vh"}}></input>
+                   <input onChange={(e)=>handle(e)} id="last_name"value={createdipendenti.last_name}className="form-control" placeholder="cognome" style={{marginTop:"1vh"}}></input>
+                   <input onChange={(e)=>handle(e)} id="cf"value={createdipendenti.cf}className="form-control" placeholder="codice fiscale" style={{marginTop:"1vh"}}></input>
+                   <input onChange={(e)=>handle(e)} id="iban"value={createdipendenti.iban}className="form-control" placeholder="iban" style={{marginTop:"1vh"}}></input>
+                   <input onChange={(e)=>handle(e)} id="id_contractType"value={createdipendenti.id_contractType}className="form-control" placeholder="tipo contratto" style={{marginTop:"1vh"}}></input>
+                   <input onChange={(e)=>handle(e)} id="email"value={createdipendenti.email}className="form-control" placeholder="email" style={{marginTop:"1vh"}}></input>
+                   <input onChange={(e)=>handle(e)} id="phoneNumber"value={createdipendenti.phoneNumber}className="form-control" placeholder="telefono" style={{marginTop:"1vh"}}></input>
+                  </form>
+                </Modal.Body>
+                <Modal.Footer>
+                <Button variant="success" type="submit">
+                        Conferma
+                    </Button>
+                    <Button variant="danger" onClick={handleClose}>
+                        Annulla
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+  
+</div>
 
 <motion.div initial={{x : 100}} animate={{x : 0}} style={{ height: 400, width: '100%' }} className='custom-grid'>
             <DataGrid
@@ -72,9 +156,14 @@ const columns: GridColDef[] = [
                 }}
             />
   </motion.div>
+
+
+  </>
    
 
     )
   }
 
   export default ListaDipendenti
+
+
