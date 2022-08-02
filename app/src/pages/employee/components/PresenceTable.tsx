1454f97a5c935c48d9e26794f6eac4bed1fd97e8
Alessandro Rocco <107470489+skaysrei@users.noreply.github.com>
2022-08-02 13:22:20 +0200

    CRUD Improvement in Employee/Presenze

--- app/src/pages/employee/components/PresenceTable.tsx
+++ app/src/pages/employee/components/PresenceTable.tsx
@@ -1,7 +1,27 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
-import { DataGrid, GridColDef, GridRowModes, GridRowModesModel, GridRowsProp, GridToolbarColumnsButton, GridToolbarContainer, GridToolbarDensitySelector, GridToolbarExport, GridToolbarFilterButton } from '@mui/x-data-grid';
+import { 
+    DataGrid, 
+    GridActionsCellItem, 
+    GridColDef, 
+    GridColumns, 
+    GridRowId, 
+    GridRowModes, 
+    GridRowModesModel, 
+    GridRowParams, 
+    GridRowsProp, 
+    GridToolbarColumnsButton, 
+    GridToolbarContainer, 
+    GridToolbarDensitySelector, 
+    GridToolbarExport, 
+    GridToolbarFilterButton, 
+    MuiEvent
+} from '@mui/x-data-grid';
 import AddIcon from "@mui/icons-material/Add";
+import SaveIcon from "@mui/icons-material/Save";
+import EditIcon from "@mui/icons-material/Edit";
+import CancelIcon from "@mui/icons-material/Close";
+import DeleteIcon from "@mui/icons-material/DeleteOutlined";
 import { motion } from 'framer-motion';
 
 import "../styles/PresenceTable.css";
@@ -16,81 +36,82 @@ const types: { [key: string]: string } = {
     'b867b283-38a0-4eb3-8df1-55ccb5f310df': 'Malattia'
 }
 
-const heading: GridColDef[] = [
-    {   field: 'date_presence',
-        headerName: 'Data',
-        type: 'date',
-        width: 279,
-        editable: true,
-        align: 'right',
-        headerAlign: 'right'
-    },
-    {   field: 'hours',
-        headerName: 'Ore',
-        type: 'number',
-        width: 279,
-        editable: true,
-        align: 'right',
-        headerAlign: 'right'
-    },
-    {   field: 'type',
-        headerName: 'Tipo Presenza',
-        type: 'string',
-        width: 279,
-        editable: true,
-        align: 'right',
-        headerAlign: 'right'
-    },
-]
+const PresenceTable = (props:any)=> {
+    const initialState: GridRowsProp = [];
+    const [presenze, setPresenze] = useState([]);
+    const [open, setOpen] = useState<any>(false);
+    const [IDRowToDelete, setIDRowToDelete] = useState<GridRowId>();
+    const [rowsBuffer, setRowsBuffer] = useState<GridRowsProp>([]);
+    const [rowsMode, setRowsMode] = useState<GridRowModesModel>({});
 
-interface EditToolbarProps {
-    setRows: (newRows: (oldRows: GridRowsProp) => GridRowsProp) => void;
-    setRowModesModel: (
-      newModel: (oldModel: GridRowModesModel) => GridRowModesModel
-    ) => void;
-  }
-  
-    function AddRow(props: EditToolbarProps) {
-    const { setRows, setRowModesModel } = props;
-    const handleClick = () => {
-        const id = randomId();
-        setRows((oldRows) => [...oldRows, { id, name: "", age: "", isNew: true }]);
-        setRowModesModel((oldModel) => ({
-            ...oldModel,
-            [id]: { mode: GridRowModes.Edit, fieldToFocus: "name" },
-        }));
-    };
-
-    return (
-        <GridToolbarContainer>
-        <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
-            AGGIUNGI
-        </Button>
-        </GridToolbarContainer>
-    );
-}
-
-function CustomToolbar() {
-    return (
-      <GridToolbarContainer>
-        <GridToolbarColumnsButton />
-        <GridToolbarFilterButton />
-        <GridToolbarDensitySelector />
-        <GridToolbarExport />
-        <Button color="primary" startIcon={<AddIcon />} onClick={handleAdd}>
-            AGGIUNGI
-        </Button>
-      </GridToolbarContainer>
-    );
-}
-
-function handleAdd() {
-
-}
-
-const PresenceTable =(props:any)=> {
-  const [presenze, setPresenze] = useState([])
-  const [localBuffer, setBuffer] = useState([])
+    const heading: GridColumns = [
+        {   field: 'date_presence',
+            headerName: 'Data',
+            type: 'date',
+            width: 279,
+            editable: true,
+            align: 'right',
+            headerAlign: 'right'
+        },
+        {   field: 'hours',
+            headerName: 'Ore',
+            type: 'number',
+            width: 279,
+            editable: true,
+            align: 'right',
+            headerAlign: 'right'
+        },
+        {   field: 'type',
+            headerName: 'Tipo Presenza',
+            type: 'string',
+            width: 279,
+            editable: true,
+            align: 'right',
+            headerAlign: 'right'
+        },
+        {
+            field: "actions",
+            type: "actions",
+            headerName: "Actions",
+            width: 100,
+            cellClassName: "actions",
+            getActions: ({ id }) => {
+              const isInEditMode = rowsMode[id]?.mode === GridRowModes.Edit;
+              if (isInEditMode) {
+                return [
+                  <GridActionsCellItem
+                    icon={<SaveIcon />}
+                    label="Save"
+                    onClick={handleSaveClick(id)}
+                  />,
+                  <GridActionsCellItem
+                    icon={<CancelIcon />}
+                    label="Cancel"
+                    className="textPrimary"
+                    onClick={handleCancelClick(id)}
+                    color="inherit"
+                  />,
+                ];
+              }
+      
+              return [
+                <GridActionsCellItem
+                  icon={<EditIcon />}
+                  label="Edit"
+                  className="textPrimary"
+                  onClick={handleEditClick(id)}
+                  color="inherit"
+                />,
+                <GridActionsCellItem
+                  icon={<DeleteIcon />}
+                  label="Delete"
+                  onClick={() => handleOpen(id)}
+                  color="inherit"
+                />,
+              ];
+            },
+          }
+    ]
 
     async function getPresenze() {
         try {
@@ -117,35 +138,99 @@ const PresenceTable =(props:any)=> {
     useEffect(()=> {
         getPresenze();
       }, [])
- 
-  return (
-    <motion.div initial={{x : 100}}
-                animate={{x : 0}}
-                className='custom-grid'
-                style={{ height: 400, width: '100%' }}>
-        <DataGrid
-            // components={{
-            //     LoadingOverlay: LinearProgress
-            // }}
-            // loading
-            rows={presenze}
-            columns={heading}
-            pageSize={5}
-            editMode='row'
-            rowsPerPageOptions={[5]}
-            checkboxSelection
-            sx={{
-                boxShadow: 20
-            }}
-            components={{
-                Toolbar: CustomToolbar
-            }}
-/*             editable={{
-                onRowAdd: (),
-                onRowEdit: (),
-                onRowDelete: ()
-            }} */
-        />
+
+    // Handlers ----------------------------------------------------------------------------|
+    const handleAdd =() => {
+        const id = randomId();
+        setRowsBuffer((rowsBuffer) => [...rowsBuffer, { id, name: "", age: "", isNew: true }]);
+        setRowsMode((rowsMode) => ({
+            ...rowsMode,
+            [id]: { mode: GridRowModes.Edit, fieldToFocus: "hours" },
+        }));
+    };
+
+    const handleRowEditStart = (
+        params: GridRowParams,
+        event: MuiEvent<React.SyntheticEvent>) => {
+        event.defaultMuiPrevented = true;
+    };
+
+    const handleRowEditStop =()=> {
+
+    }
+
+    const handlePushChanges =()=> {
+
+    }
+
+    const handleEditClick = (id: GridRowId) => () => {
+        setRowsMode({ ...rowsMode, [id]: { mode: GridRowModes.Edit } });
+    };
+    
+    const handleSaveClick = (id: GridRowId) => () => {
+        setRowsMode({ ...rowsMode, [id]: { mode: GridRowModes.View } });
+    };
+
+    const handleCancelClick = (id: GridRowId) => () => {
+        setRowsMode({
+          ...rowsMode,
+          [id]: { mode: GridRowModes.View, ignoreModifications: true },
+        });
+    
+        const editedRow = rowsBuffer.find((row) => row.id === id);
+        if (editedRow!.isNew) {
+          setRowsBuffer(rowsBuffer.filter((row) => row.id !== id));
+        }
+    };
+
+    const handleOpen = (id: GridRowId) => {
+        setOpen(true);
+        setIDRowToDelete(id);
+    };
+    
+
+    const CustomToolbar = ()=> {
+        return(
+            <GridToolbarContainer>
+            <GridToolbarColumnsButton />
+            <GridToolbarFilterButton />
+            <GridToolbarDensitySelector />
+            <GridToolbarExport />
+            <Button color="primary" startIcon={<AddIcon />} onClick={handleAdd}>
+                AGGIUNGI
+            </Button>
+            </GridToolbarContainer>
+        );
+    }
+
+    return (
+        <motion.div 
+            initial={{x : 100}}
+            animate={{x : 0}}
+            className='custom-grid'
+            style={{ height: 400, width: '100%' }}>
+            <DataGrid
+                // components={{
+                //     LoadingOverlay: LinearProgress
+                // }}
+                // loading
+                rows={rowsBuffer}
+                columns={heading}
+                pageSize={5}
+                editMode='row'
+                rowModesModel={rowsMode}
+                onRowEditStart={handleRowEditStart}
+                rowsPerPageOptions={[20]}
+                checkboxSelection
+                components={{
+                    Toolbar: CustomToolbar
+                }}
+    /*             editable={{
+                    onRowAdd: (),
+                    onRowEdit: (),
+                    onRowDelete: ()
+                }} */
+            />
   </motion.div>
    )
 }
