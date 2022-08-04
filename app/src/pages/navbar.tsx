280d075268f8e43190f7d92150b8adcb7a38af3c
Lorenzo <lorprofessional@gmail.com>
2022-08-04 16:11:05 +0200

    cambiamento aspetto navbar collapsed

--- app/src/pages/navbar.tsx
+++ app/src/pages/navbar.tsx
@@ -2,6 +2,7 @@ import { Navbar, Container, Nav, Badge } from "react-bootstrap";
 import { Outlet, useNavigate } from "react-router-dom";
 import { Offcanvas } from "react-bootstrap";
 import { useState } from "react";
+import './navbar.css'
 
 export default function TopMenu() {
   const navigate = useNavigate();
@@ -11,18 +12,18 @@ export default function TopMenu() {
   }
   const brand = ( 
 	<>
-		<Navbar.Brand href="/">
-			<img
-			src="/statics/Risorsa 22.png"
-			className="d-inline-block align-top"
-			width="auto"
-			height="40"
-			alt="React Bootstrap logo"
-			/>
-			Cloudmark
-		</Navbar.Brand>
-		<Badge>{sessionStorage.business_name}</Badge>
-                </>
+    <Navbar.Brand href="/">
+      <img
+      src="/statics/Risorsa 22.png"
+      className="d-inline-block align-top"
+      width="auto"
+      height="40"
+      alt="React Bootstrap logo"
+      />
+      Cloudmark
+    </Navbar.Brand>
+    <Badge>{sessionStorage.business_name}</Badge>
+  </>
   )
   return (
     <>
@@ -35,14 +36,14 @@ export default function TopMenu() {
               aria-labelledby="offcanvasNavbar-expand-lg"
               placement="start"
             >
-          <Navbar.Collapse   id="responsive-navbar-nav">
+          <Navbar.Collapse   id="responsive-navbar-nav" className="bg-dark">
           <Offcanvas.Header closeButton onClick={toggleCollapse}>
             {collapse && brand}
           </Offcanvas.Header>
             <Nav className="me-auto">
               {/* <Nav.Link href="/employee">Dipendente</Nav.Link> */}
-              <Nav.Link href="/admin">Admin</Nav.Link>
-              <Nav.Link href="/superuser">Superuser</Nav.Link>
+              <Nav.Link href="/admin" className="adm">Admin</Nav.Link>
+              <Nav.Link href="/superuser" className="superuse">Superuser</Nav.Link>
             </Nav>
             <Nav className="d-flex">
               {sessionStorage.bearer ? (
@@ -53,10 +54,11 @@ export default function TopMenu() {
                       sessionStorage.clear();
                       navigate("/login");
                     }}
+                    className="login"
                   >
                     logout
                   </Nav.Link>
-                  <Nav.Link href="/profile">
+                  <Nav.Link href="/profile" className="prof">
                     {sessionStorage.user}
                   </Nav.Link>
                 </>
