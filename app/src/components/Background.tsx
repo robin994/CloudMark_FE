5303a7134aa6155c94e9f88b7012f71061366a5b
Lorenzo <lorprofessional@gmail.com>
2022-08-04 16:49:26 +0200

    fix shape rendering error

--- app/src/components/Background.tsx
+++ app/src/components/Background.tsx
@@ -7,7 +7,7 @@ const Background = (props: any) => {
         <div className="inner-header flex">
           </div>
           <div>
-            <svg className="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+            <svg className="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
               <defs>
                 <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
               </defs>
