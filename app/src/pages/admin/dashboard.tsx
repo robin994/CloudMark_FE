749c366f85ed373916ca8587c5d308d75d7d0d3c
Lorenzo <lorprofessional@gmail.com>
2022-08-08 16:01:02 +0200

    fixed view dashboard

--- app/src/pages/admin/dashboard.tsx
+++ app/src/pages/admin/dashboard.tsx
@@ -10,7 +10,7 @@ textDesc[0] = "Operazioni sui Dipendenti";
 textDesc[1] = "Lista di tutti i dipendenti";
 textDesc[2] = "Ricerca di un singolo cliente";
 textDesc[3] = "Lista di tutti i clienti";
-textDesc[4] = "Visualizzare le presenze tramite il calendario";
+textDesc[4] = "Visualizza l'andamento dei prezzi delle commesse";
 
 export default function Dashboard() {
   return (
@@ -25,7 +25,7 @@ export default function Dashboard() {
           transform: "translate(50%, -50%)",
         }}
       >
-        <Row className="my-5 justify-content-center align-items-center">
+        <Row className="mt-10 justify-content-center align-items-center d-grid rowCont">
           <motion.div
             className="col-3 d-flex justify-content-center"
             initial={{ x: -100 }}
@@ -77,6 +77,20 @@ export default function Dashboard() {
               navPath="/commesse"
               buttonText="Mostra"
               textDesc={textDesc[3]}
+            />
+          </motion.div>
+
+          <motion.div
+            className="col-3 d-flex justify-content-center"
+            initial={{ y: 100 }}
+            animate={{ y: 0 }}
+          >
+            <CustomCard
+              cardTitle="Grafico Commesse"
+              imgSrc="https://www.svgrepo.com/show/35765/graph.svg"
+              navPath="/chart"
+              buttonText="Mostra"
+              textDesc={textDesc[4]}
             />
           </motion.div>
         </Row>
