182d7d7e24e6285f489dd0b54b056c2ab9c061ad
francescolillo <francescolillo@hotmail.it>
2022-08-10 16:40:40 +0200

    aggiunta dei mesi mancanti

--- app/src/pages/admin/components/chart/Chart.jsx
+++ app/src/pages/admin/components/chart/Chart.jsx
@@ -65,7 +65,7 @@ import {
 
         useEffect(() => {
             setChartData({
-                labels: ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio","Giugno","Luglio", "Agosto", "Settembre"],
+                labels: ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio","Giugno","Luglio", "Agosto", "Settembre","Ottobre","Novembre","Dicembre"],
                 datasets: [
                     {
                         label: "chart alpha test",
