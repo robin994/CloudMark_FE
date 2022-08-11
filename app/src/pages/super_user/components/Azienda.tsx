1dd94d4808e76ce7a96fe79d5c9ee2d164718a55
Roberto <robin994@hotmail.it>
2022-08-11 09:54:49 +0200

    Revert "Merge branch 'Evolutiva' of https://github.com/robin994/CloudMark_FE into Evolutiva"
    This reverts commit 364dc62a5d05f610e75a5a442c7005f439820e40, reversing
    changes made to 182d7d7e24e6285f489dd0b54b056c2ab9c061ad.

--- app/src/pages/super_user/components/Azienda.tsx
+++ app/src/pages/super_user/components/Azienda.tsx
@@ -1,25 +1,11 @@
 import * as React from 'react';
 import Box from "@mui/material/Box";
 import {
-    DataGrid,
-    GridActionsCellItem,
-    GridColumns,
-    GridEventListener,
-    GridRowId,
-    GridRowModel,
-    GridRowModes,
-    GridRowModesModel,
-    GridRowParams,
-    GridRowsProp,
-    MuiEvent,
+  DataGrid,
+  GridColumns,
+  GridRowsProp,
 } from "@mui/x-data-grid";
-import EditIcon from "@mui/icons-material/Edit";
-import SaveIcon from "@mui/icons-material/Save";
-import CancelIcon from "@mui/icons-material/Close";
-import DeleteIcon from "@mui/icons-material/DeleteOutlined";
-import { Button, Fade, Typography } from "@mui/material";
 import axios from "axios";
-import Axios from 'axios';
 
 const initialRows: GridRowsProp = [];
 
@@ -27,7 +13,6 @@ const Azienda = () => {
 
     const [pageSize, setPageSize] = React.useState<number>(14);
     const [rows, setRows] = React.useState(initialRows);
-    const [rowModesModel, setRowModesModel] = React.useState<GridRowModesModel>({});
 
     async function getAziende() {
         axios
@@ -55,238 +40,83 @@ const Azienda = () => {
         });
     }
 
-    const processRowUpdate = (newRow: GridRowModel) => {
-        const updatedRow = { ...newRow, isNew: false };
-        Axios(
-            `${process.env.REACT_APP_FASTAPI_URL}/business/update/`,
-            {
-                method: 'POST',
-                headers: { 
-                    accept: 'application/json',
-                },
-                data: {
-                    name: updatedRow.name,
-                    p_iva: updatedRow.p_iva,
-                    address: updatedRow.address,
-                    cap: updatedRow.cap,
-                    iban: updatedRow.iban,
-                    phone: updatedRow.phone,
-                    email: updatedRow.email,
-                    pec: updatedRow.pec,
-                    fax: updatedRow.fax,
-                    id_business: updatedRow.id_business
-                }
-            }
-            
-        ).catch((err) => {
-            console.log(err);
-        })
-
-        setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
-        return updatedRow;
-    };
-
-    const handleDeleteClick = () => () => {
-        let id: GridRowId = "";
-        let id_business = "";
-        if (IDRowToDelete !== undefined) {
-            id = IDRowToDelete;
-            for (let row of rows) {
-                if (row["id"] === id) {
-                    id_business = row["id_business"];
-                }
-            }
-            axios
-            .request({
-                url: `${process.env.REACT_APP_FASTAPI_URL}/business/delete/`,
-                method: "post",
-                params: {
-                    id_business: id_business,
-                },
-            }).then(() => {
-                setRows(rows.filter((row) => row.id !== id));
-                setOpen(false);
-            });
-        }
-    };
-
-    const handleRowEditStart = (
-        params: GridRowParams,
-        event: MuiEvent<React.SyntheticEvent>
-    ) => {
-        event.defaultMuiPrevented = true;
-    };
-
-    const handleRowEditStop: GridEventListener<"rowEditStop"> = (
-        params,
-        event
-    ) => {
-        event.defaultMuiPrevented = true;
-    };
-
 
     React.useEffect(() => {
         getAziende();
-    }, []);
-
-    const handleEditClick = (id: GridRowId) => () => {
-        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
-    };
-
-    const handleSaveClick = (id: GridRowId) => () => {
-        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
-    };
-
-    const handleCancelClick = (id: GridRowId) => () => {
-        setRowModesModel({
-            ...rowModesModel,
-            [id]: { mode: GridRowModes.View, ignoreModifications: true },
-        });
-
-        const editedRow = rows.find((row) => row.id === id);
-        if (editedRow!.isNew) {
-            setRows(rows.filter((row) => row.id !== id));
-        }
-    };
+      }, []);
 
     const columns: GridColumns = [
         {
             field: "id_business",
             headerName: "id azienda",
-            width: 200,
-            editable: true,
+            width: 210,
+            editable: false,
             hide: true,
         },
         {
             field: "name",
             headerName: "nome azienda",
-            width: 200,
-            editable: true,
+            width: 210,
+            editable: false,
             hide: false,
         },
         {
             field: "p_iva",
             headerName: "partita iva",
-            width: 200,
-            editable: true,
+            width: 210,
+            editable: false,
             hide: false,
         },
         {
             field: "address",
             headerName: "indirizzo",
-            width: 200,
-            editable: true,
+            width: 210,
+            editable: false,
             hide: false,
         },
         {
             field: "cap",
             headerName: "cap",
-            width: 200,
-            editable: true,
+            width: 210,
+            editable: false,
             hide: false,
         },
         {
             field: "iban",
             headerName: "iban",
-            width: 200,
-            editable: true,
+            width: 210,
+            editable: false,
             hide: false,
         },
         {
             field: "phone",
             headerName: "numero di telefono",
-            width: 200,
-            editable: true,
+            width: 210,
+            editable: false,
             hide: false,
         },
         {
             field: "email",
             headerName: "email",
-            width: 200,
-            editable: true,
+            width: 210,
+            editable: false,
             hide: false,
         },
         {
             field: "pec",
             headerName: "pec",
-            width: 200,
-            editable: true,
+            width: 210,
+            editable: false,
             hide: false,
         },
         {
             field: "fax",
             headerName: "fax",
-            width: 200,
-            editable: true,
+            width: 210,
+            editable: false,
             hide: false,
         },
-        {
-            field: "actions",
-            type: "actions",
-            headerName: "Actions",
-            width: 100,
-            cellClassName: "actions",
-            getActions: ({ id }) => {
-                const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
-
-                if (isInEditMode) {
-                    return [
-                        <GridActionsCellItem
-                            icon={<SaveIcon />}
-                            label="Save"
-                            onClick={handleSaveClick(id)}
-                        />,
-                        <GridActionsCellItem
-                            icon={<CancelIcon />}
-                            label="Cancel"
-                            className="textPrimary"
-                            onClick={handleCancelClick(id)}
-                            color="inherit"
-                        />,
-                    ];
-                }
-
-                return [
-                    <GridActionsCellItem
-                        icon={<EditIcon />}
-                        label="Edit"
-                        className="textPrimary"
-                        onClick={handleEditClick(id)}
-                        color="inherit"
-                    />,
-                    <GridActionsCellItem
-                        icon={<DeleteIcon />}
-                        label="Delete"
-                        onClick={() => handleOpen(id)}
-                        color="inherit"
-                    />,
-                ];
-            },
-        },
     ]
-
-    const style = {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        borderRadius: "10px",
-        position: "absolute" as "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        width: 400,
-        bgcolor: "background.paper",
-        boxShadow: 24,
-        p: 4,
-    };
-
-    const [open, setOpen] = React.useState<any>(false);
-    const [IDRowToDelete, setIDRowToDelete] = React.useState<GridRowId>();
-    const handleOpen = (id: GridRowId) => {
-        setOpen(true);
-        setIDRowToDelete(id);
-    };
        
   return (
     <Box
@@ -308,44 +138,7 @@ const Azienda = () => {
             pagination
             rows={rows}
             columns={columns}
-            editMode="row"
-            rowModesModel={rowModesModel}
-            onRowEditStart={handleRowEditStart}
-            onRowEditStop={handleRowEditStop}
-            processRowUpdate={processRowUpdate}
-            experimentalFeatures={{ newEditingApi: true }}
         />
-        {open && (
-            <div>
-                <Fade in={open}>
-                    <Box sx={style}>
-                    <Typography
-                        id="transition-modal-title"
-                        variant="h6"
-                        component="h2"
-                    >
-                        Vuoi cancellarlo?
-                    </Typography>
-                    <div style={{ display: "flex" }}>
-                        <Button
-                        onClick={handleDeleteClick()}
-                        style={{ margin: "10px", height: "40px", width: "90px" }}
-                        variant="outlined"
-                        >
-                        SI
-                        </Button>
-                        <Button
-                        onClick={() => setOpen(false)}
-                        style={{ margin: "10px", height: "40px", width: "90px" }}
-                        variant="outlined"
-                        >
-                        NO
-                        </Button>
-                    </div>
-                    </Box>
-                </Fade>
-            </div>
-        )}
     </Box>
   );
 }
